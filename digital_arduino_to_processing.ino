e36d2d009f7d25ef38bd7d3686b27a9ffcecb421
Lucagiac <giacoliniluca2@gmail.com>
2016-10-13 20:39:27 +0100

    Update digital_arduino_to_processing.ino

--- digital_arduino_to_processing.ino
+++ digital_arduino_to_processing.ino
@@ -7,17 +7,21 @@
 // Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 
 // INTRODUCTION
-//sending input value from digitalPin2, digitalPin3 Arduino to Processing through serial communication
+//sending DIGITAL values from Arduino to Processing through serial communication
 
 // HARDWARE
-// two push buttons each individually grounded through a pull-up resistor (10kOhm)
+// four push buttons each individually grounded through a pull-up resistor (10kOhm)
 
 
-#define buttonPin        2
-#define buttonPin1       3
+#define buttonPin        7
+#define buttonPin1       2
+#define buttonPin2       3
+#define buttonPin3       4
  
-boolean previousState; // previous state
+boolean previousState;
 boolean previousState1;
+boolean previousState2;
+boolean previousState3;
 
 void setup()
 {
@@ -26,9 +30,16 @@ void setup()
   pinMode(buttonPin1, INPUT);
   digitalWrite(buttonPin1, HIGH); // pull-up
   Serial.begin(9600);
+  pinMode(buttonPin2, INPUT);
+  digitalWrite(buttonPin2, HIGH); // pull-up
+  pinMode(buttonPin3, INPUT);
+  digitalWrite(buttonPin3, HIGH); // pull-up
+  Serial.begin(9600);
   //setting the booleans as false
   previousState = false;
   previousState1 = false;
+  previousState2 = false;
+  previousState3 = false;
 }
 
 void loop()
@@ -48,6 +59,20 @@ void loop()
   if (value1)
   { 
     Serial.write(1);
+  }
+
+  // ask the third button whether it is true or false. if pressed send a 2
+  boolean value2 = handle_button2();
+  if (value2)
+  { 
+    Serial.write(2);
+  }
+
+  // ask the fourth button whether it is true or false. if pressed send a 3
+  boolean value3 = handle_button3();
+  if (value3)
+  { 
+    Serial.write(3);
   }
   
   delay(10);
@@ -72,4 +97,24 @@ boolean handle_button1()
   event1 = currentState1 && !previousState1;
   previousState1 = currentState1;
   return event1;
+}
+
+boolean handle_button2()
+{
+  boolean event2;
+  int currentState2 = !digitalRead(buttonPin2); // pin low -> value
+
+  event2 = currentState2 && !previousState2;
+  previousState2 = currentState2;
+  return event2;
+}
+
+boolean handle_button3()
+{
+  boolean event3;
+  int currentState3 = !digitalRead(buttonPin3); // pin low -> value
+
+  event3 = currentState3 && !previousState3;
+  previousState3 = currentState3;
+  return event3;
 }
